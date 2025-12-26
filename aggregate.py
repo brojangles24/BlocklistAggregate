3ade19d8c291d3af2cb7da9f80fd723be1d3c9ee
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2025-12-25 22:53:52 -0700

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -6,7 +6,6 @@ from datetime import datetime
 BLOCKLIST_URLS = [
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.medium-onlydomains.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate-onlydomains.txt",
-    "https://badmojr.github.io/1Hosts/Lite/domains.txt",
 ]
 TLD_LIST_URL = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/spam-tlds-onlydomains.txt"
 
