cfba41124d1e71de71efb2d16a16b0990a091e4b
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-02-16 19:28:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ CORE_SOURCES = [
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/nsfw.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/anti.piracy.txt",
     "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
-    "https://filters.adtidy.org/extension/chromium/filters/3.txt"
 ]
 SPAM_TLD_URL = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/spam-tlds.txt"
 
