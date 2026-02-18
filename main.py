c7e8be98d29b5899796cccaeee644de4ee03c654
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-02-18 09:46:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ VERSION = "2026.02.17.CORE_CLEAN_ULTIMATE"
 
 CORE_SOURCES = [
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt",
-    #"https://badmojr.github.io/1Hosts/Lite/adblock.txt",
+    "https://badmojr.github.io/1Hosts/Lite/adblock.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt",
-    #"https://big.oisd.nl",
-    #"https://nsfw.oisd.nl",
+    "https://big.oisd.nl",
+    "https://nsfw.oisd.nl",
     "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adguard/dns-rebind-protection.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/social.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/nsfw.txt",
