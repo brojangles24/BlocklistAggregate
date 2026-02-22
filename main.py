1b84a4bc444b21bcd2f61f855ad31f1117c687ad
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-02-22 13:12:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def main():
         f.write("! --- HAGEZI SPAM TLDs (RAW) ---\n")
         f.write("\n".join(spam_tld_raw) + "\n\n")
         f.write("! --- CUSTOM ENFORCEMENT & SAFESEARCH ---\n")
-        #f.write(YOUTUBE_RULE + "\n\n")
+        f.write(YOUTUBE_RULE + "\n\n")
         f.write(f"! --- NSFW REGEX RULE ---\n")
         f.write(f"/{NSFW_REGEX.pattern}/\n")
 
