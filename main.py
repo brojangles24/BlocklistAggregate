e7e6dbdc86c88545728494c7ef177be9cb3574cd
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-03-12 09:25:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ DEFAULT_SOURCES = [
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.mini.txt", # Threat Intel Mini ~ 150k
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/ultimate.txt", # Hagezi Ultimate ~ 300k
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt", # Hagezi Pro++ ~ 250k
-    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt", # Hagezi Pro ~ 200k
+    #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt", # Hagezi Pro ~ 200k
+    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/ultimate.mini.txt", #Hagezi Ultimate Mini ~ 110k
     "https://badmojr.github.io/1Hosts/Lite/adblock.txt", # 1Hosts Lite ~ 95k
     #"https://big.oisd.nl", # OISD Full ~ 300k
     #"https://nsfw.oisd.nl", # OISD NSFW Full ~ 300k
-    #"https://nsfw-small.oisd.nl", #OISD NSFW Small ~ 25k
+    "https://nsfw-small.oisd.nl", #OISD NSFW Small ~ 25k
     "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adguard/dns-rebind-protection.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/social.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/nsfw.txt",
