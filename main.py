9d596cabdda4d66a07ce62813c89221be0e4d961
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-03-12 21:18:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ AZ_TZ = timezone(timedelta(hours=-7))
 VERSION = "2026.03.12.FINAL_CLEAN_FULL"
 
 DEFAULT_SOURCES = [
-    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt",
+    #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt",
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.medium.txt",
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.mini.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/ultimate.txt",
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt",
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt",
     #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/ultimate.mini.txt",
-    "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Xtra/adblock.txt",
-    #"https://badmojr.github.io/1Hosts/Lite/adblock.txt",
+    #"https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Xtra/adblock.txt",
+    "https://badmojr.github.io/1Hosts/Lite/adblock.txt",
     "https://big.oisd.nl",
     "https://nsfw.oisd.nl",
     #"https://nsfw-small.oisd.nl",
