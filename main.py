0d45d9ddbb5a7273b4911ba28e7ca9b5d95d69ba
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-05-27 12:57:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ MAIN_SOURCES = [
     # --- 1. Content Filtering & Core Safety Baseline ---
     "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/abp_nsfw.txt",
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
-    "https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
-    "https://raw.githubusercontent.com/Bon-Appetit/porn-domains/main/block.txt",
+    #"https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
+    #"https://raw.githubusercontent.com/Bon-Appetit/porn-domains/main/block.txt",
 
     # --- 2. The Multi-Threat Security Core (The "Mini-Omni" Engines) ---
     #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/dyndns.txt",
