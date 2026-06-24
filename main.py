d83823f11b89d528cbb334a0c68ff35718d0ed6d
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-06-24 08:16:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ MAIN_SOURCES = [
     # --- 1. Content Filtering & Core Safety Baseline ---
     "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/abp_nsfw.txt",
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
-    #"https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
+    "https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
 
     # --- 2. Enforcement & Behavioral Blocks ---
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/social.txt",
