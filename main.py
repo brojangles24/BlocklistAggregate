3606659c1c7c6c8cf0820e05c23251ced92409e0
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-06-26 06:00:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ ADBLOCK_BASIC_RE = re.compile(r'^([^/\^]+)\^')
 MAIN_SOURCES = [
     # --- 1. Content Filtering & Core Safety Baseline ---
     "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/abp_nsfw.txt",
-    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
-    "https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
+    #"https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
+    #"https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt",
 
     # --- 2. Enforcement & Behavioral Blocks ---
