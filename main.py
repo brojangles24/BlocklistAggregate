b867bcebfba5b3038099538735e8c46f0395e01e
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-07-18 22:09:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ MAIN_SOURCES = [
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/social.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/nsfw.txt",
     "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/nosafesearch.txt",
-    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/fake.txt",
+    #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/fake.txt",
 ]
 
 # ---------------------------------------------------------------------------
